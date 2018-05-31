400a34d99aa559f9bf38a20a2bed4f7eb2ea88bc
Leo Orellana <leoorellana@lawn-143-215-52-64.lawn.gatech.edu>
2018-05-31 18:34:54 -0400

    update5.31

---
+++ public/assets/js/skills.js
@@ -0,0 +1,8 @@
+// $(function () {
+//     $(".submit").on("click",function (event) {
+//         var id = $(this).data("id");
+//         var skills = $(this).data()
+//     })
+//     var id = $(this).data('id');
+//     var
+// })
