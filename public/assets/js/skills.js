54a5e7405ef633e6d664bbba9d69df3b8f84e913
Leo Orellana <leoorellana@Leos-MacBook-Air.local>
2018-05-31 20:13:18 -0400

    nothing

--- public/assets/js/skills.js
+++
@@ -1,8 +0,0 @@
-// $(function () {
-//     $(".submit").on("click",function (event) {
-//         var id = $(this).data("id");
-//         var skills = $(this).data()
-//     })
-//     var id = $(this).data('id');
-//     var
-// })
