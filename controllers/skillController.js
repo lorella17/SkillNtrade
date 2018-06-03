674d91998e3d3de380c2fb9bb8d8541d1263bdba
unknown <jjernigan1065@gmail.com>
2018-06-03 12:06:53 -0400

    testing app with mailgun for heroku deployment

--- controllers/skillController.js
+++ controllers/skillController.js
@@ -92,12 +92,27 @@ router.post('/api/skills', (req, res) => {
         return dbWants;
 
 
-    }).catch(err => {
-        console.error(err);
+    }).then(dbWants => {
+        console.log(`email: ${dbWants.email}`);
+        const api_key = process.env.MAILGUN_API_KEY;
+        const domain = process.env.MAILGUN_DOMAIN;
+        const mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+        const data = {
+            from: 'Mail Gun <postmaster@sandbox1b5e6382d43947a7868b05e04f5bdc23.mailgun.org>',
+            to: `${dbWants.email}`,
+            subject: `update`,
+            text: `Your skill has been added to our site! Prepare to trade for what you need!`
+        };
+        console.log(`data: ${JSON.stringify(data, null, 2)}`);
+        mailgun.messages().send(data, function (error, body) {
+            console.log(error);
+            console.log(`body: ${body}`)
+        }).catch(err => {
+            console.error(err);
+        });
+
     });
-
 });
-
 //post route for tasks api
 router.post('/api/tasks', (req, res) => {
     console.log(req.body);
@@ -111,11 +126,27 @@ router.post('/api/tasks', (req, res) => {
         category: req.body.category
     }).then(dbNeeds => {
         res.json(dbNeeds);
-    }).catch(err => {
-        console.error(err);
+        return dbNeeds;
+    }).then(dbNeeds => {
+        console.log(`email: ${dbNeeds.email}`);
+        const api_key = process.env.MAILGUN_API_KEY;
+        const domain = process.env.MAILGUN_DOMAIN;
+        const mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+        const data = {
+            from: 'Mail Gun <postmaster@sandbox1b5e6382d43947a7868b05e04f5bdc23.mailgun.org>',
+            to: `${dbNeeds.email}`,
+            subject: `update`,
+            text: `Your skill has been added to our site! Prepare to trade for what you need!`
+        };
+        console.log(`data: ${JSON.stringify(data, null, 2)}`);
+        mailgun.messages().send(data, function (error, body) {
+            console.log(error);
+            console.log(`body: ${body}`)
+        }).catch(err => {
+            console.error(err);
+        });
     });
-});
-
+})
 module.exports = router;
 
 
