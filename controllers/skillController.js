c891707d92d5f147c61a66a724aeeaadbfeb26b7
unknown <jjernigan1065@gmail.com>
2018-06-03 11:14:47 -0400

    testing mailgun again.

--- controllers/skillController.js
+++ controllers/skillController.js
@@ -4,6 +4,8 @@ const express = require('express');
 
 const router = express.Router();
 
+const mailgun =
+
 
 //get route for main page
 router.get('/', (req, res) => {
@@ -86,16 +88,31 @@ router.post('/api/skills', (req, res) => {
         description: req.body.description
 
     }).then(dbWants => {
-        // console.log(`email: ${dbWants.email}`);
         res.json(dbWants);
+        return dbWants;
 
-
-        })
-            .catch(err => {
-            console.error(err);
+    }).then(dbWants => {
+        console.log(`email: ${dbWants.email}`);
+        // const api_key = ENV['MAILGUN_API_KEY'];
+        const api_key = 'key-72920a2e38f995ab4fc411c1300b93a4-b6183ad4-3f150811';
+        // const domain = ENV['MAILGUN_DOMAIN'];
+        const domain = 'sandbox1b5e6382d43947a7868b05e04f5bdc23.mailgun.org';
+        const mailgun = require('mailgun-js')({apiKey: api_key, domain: domain});
+        const data = {
+            from: 'Mail Gun <postmaster@sandbox1b5e6382d43947a7868b05e04f5bdc23.mailgun.org>',
+            to: `${dbWants.email}`,
+            subject: `update`,
+            text: `Your skill has been added to our site! Prepare to trade for what you need!`
+        };
+        console.log(`data: ${JSON.stringify(data, null, 2)}`);
+        mailgun.messages().send(data, function (error, body) {
+            console.log(`body: ${body}`)
         });
-    });
-// });
+    }).catch(err => {
+                console.error(err);
+            });
+
+});
 
 //post route for tasks api
     router.post('/api/tasks', (req, res) => {
